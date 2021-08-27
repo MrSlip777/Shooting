108a68b0e742965579f432ad8ac61b6be7a66e3d
slip <mrslipgamecreate01@gmail.com>
2021-08-28 08:23:22 +0900

    draft

---
+++ RotateTarget.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTarget : MonoBehaviour
+{
+
+    [SerializeField][Range(-1.0f, 1.0f)]
+    float X_Axis_RotationalSpeed = 0.0f;
+
+    [SerializeField][Range(-1.0f, 1.0f)]
+    float Y_Axis_RotationalSpeed = 0.0f;
+
+    [SerializeField][Range(-1.0f, 1.0f)]
+    float Z_Axis_RotationalSpeed = 0.0f;
+
+    Vector3 targetAngles = Vector3.zero;
+
+    //再生フラグ（初期値はtrue）
+    bool m_IsPlaying = true;
+
+    Vector3 m_InitialAngles = Vector3.zero;
+
+    public bool IsPlaying{
+        set{m_IsPlaying = value;}
+        get{return m_IsPlaying;}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetAngles = transform.eulerAngles;
+        m_InitialAngles = transform.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.IsPlaying){
+
+            targetAngles.x = targetAngles.x + X_Axis_RotationalSpeed;
+            targetAngles.y = targetAngles.y + Y_Axis_RotationalSpeed;
+            targetAngles.z = targetAngles.z + Z_Axis_RotationalSpeed;
+
+            transform.eulerAngles = targetAngles;
+        }
+    }
+
+    //リセット
+    public void Reset(){
+        transform.eulerAngles = m_InitialAngles;
+    }
+}
---
+++ RotateTargetEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RotateTarget))]//拡張するクラスを指定
+public class RotateTargetEditor : Editor {
+
+  /// <summary>
+  /// InspectorのGUIを更新
+  /// </summary>
+  public override void OnInspectorGUI(){
+    //元のInspector部分を表示
+    base.OnInspectorGUI ();
+
+    //targetを変換して対象を取得
+    RotateTarget m_RotateTarget = target as RotateTarget;
+
+    if (GUILayout.Button("再生")){
+        m_RotateTarget.IsPlaying = true;
+    }
+
+    if (GUILayout.Button("停止")){
+        m_RotateTarget.IsPlaying = false;
+    }
+
+    if (GUILayout.Button("リセット")){
+        m_RotateTarget.Reset();
+    }
+  }
+
+}
